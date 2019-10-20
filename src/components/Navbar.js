61d1ef6dcdd3f25f9c479e03d7b300c21cf17912
King Elisha <ek.chibueze@gmail.com>
2019-10-21 00:06:52 +0100

    navbar basic markup

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Logo</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Sign In</li>
+        </ul>
+      </div>
+    )
+  }
+}
