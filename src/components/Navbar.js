99fbb32f6742a663ddf4d22a6b09c5193cf295d4
King Elisha <ek.chibueze@gmail.com>
2019-10-21 13:54:12 +0100

    scale-fade

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,17 @@
+//@ts-check
 import React, { Component } from 'react';
 import './Navbar.scss';
 
 export default class Navbar extends Component {
+
+  /** @param {React.MouseEvent<HTMLDivElement>} event */
+  slideNav = (event) => {
+    const burger = event.target;
+    const links = document.querySelector("#links");
+
+
+  };
+
   render() {
     return (
       <div id="navbar">
@@ -14,7 +24,7 @@ export default class Navbar extends Component {
           <li>Sign In</li>
         </ul>
 
-        <div id="burger">
+        <div id="burger" onClick={this.slideNav}>
           <div className="line1"></div>
           <div className="line2"></div>
           <div className="line3"></div>
